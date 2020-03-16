c23fd376a11dc0378250fc5898d232f5ac5c7413
Eric Houdek <61798091+ehoudek@users.noreply.github.com>
2020-03-16 15:40:01 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix and cacheSolve that work in tandem 
+## to return a cache that is an inverse of a specified matrix 
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that creates a matrix object that is able to cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        mtrix <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                mtrix <<- NULL
 }
 get <- function () x
-        setinverse <- function(inverse) m <<- inverse 
-        getinverse <- function() m
+        setinverse <- function(inverse) mtrix <<- inverse 
+        getinverse <- function() mtrix
         list(set = set, get = get, 
-             setinverse = setinverse
+             setinverse = setinverse,
              getinverse = getinverse)
 }
 
-## Write a short comment describing this function
+## cacheSolve is a function that computes the inverse of the matrix created by 'makeCacheMatrix'. 
+## If the inverse has been calculated (and not changed), then 'cacheSolve' will return the inverse from the cache. 
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
+        mtrix <- x$getinverse()
+        if(!is.null(mtrix)) {
                 message("getting cached data")
-                return(m)
+                return(mtrix)
         }
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        mtrix <- solve(data, ...)
+        x$setinverse(mtrix)
+        mtrix
 }
+
+## *************TEST********************************
+ testm <- matrix(rnorm(27),3,3)
+ testm1 <- makeCacheMatrix(testm)
+ cacheSolve(testm1)
